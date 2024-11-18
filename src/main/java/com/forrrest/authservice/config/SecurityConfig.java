7c57e07562189d221bc9f57a7313a21704fd7511
beomkyu choi <beomkyuchoi069@gmail.com>
2024-11-18 14:57:17 +0900

    TokenProvider 의존성 명시적 표현

--- src/main/java/com/forrrest/authservice/config/SecurityConfig.java
+++ src/main/java/com/forrrest/authservice/config/SecurityConfig.java
@@ -13,14 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.forrrest.common.security.filter.UserTokenFilter;
-import com.forrrest.common.security.token.TokenProvider;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final TokenProvider tokenProvider;
     private final UserTokenFilter userTokenFilter;
 
     @Bean
