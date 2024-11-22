6778edfb6cfbe90c3373dc6e3721a02ee2727588
beomkyu choi <beomkyuchoi069@gmail.com>
2024-11-22 10:36:39 +0900

    a

--- src/main/java/com/forrrest/authservice/repository/ProfileRepository.java
+++ src/main/java/com/forrrest/authservice/repository/ProfileRepository.java
@@ -3,10 +3,12 @@ package com.forrrest.authservice.repository;
 import com.forrrest.authservice.entity.Profile;
 import com.forrrest.authservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     List<Profile> findAllByUser(User user);
     Optional<Profile> findByIdAndUser(Long id, User user);
--- src/main/java/com/forrrest/authservice/repository/RefreshTokenRepository.java
+++ src/main/java/com/forrrest/authservice/repository/RefreshTokenRepository.java
@@ -2,9 +2,11 @@ package com.forrrest.authservice.repository;
 
 import com.forrrest.authservice.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {  // ID 타입을 String으로 변경
     Optional<RefreshToken> findByRefreshToken(String token);
 }
